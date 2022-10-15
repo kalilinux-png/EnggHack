4e03afa804c6bfc1d5421bc920141fb219673f5c
kalilinux-png <kalilinuxuser5@gmail.com>
2022-10-15 07:48:58 +0530

    Error: line 34 slice failed

--- teach.py
+++ teach.py
@@ -1,42 +1,81 @@
-import time
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
 import cv2
 from keras.models import load_model
-from PIL import Image, ImageOps
 import numpy as np
 
 
-# cam= cv2.VideoCapture('video_Trim.mp4') # read 
-cam = cv2.VideoCapture(0)
+facedetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap=cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+font=cv2.FONT_HERSHEY_COMPLEX
+
+
 model = load_model('keras_model.h5')
 
+
+def get_className(classNo):
+	if classNo==0:
+		return "shubham"
+	elif classNo==1:
+		return "sudarshan"
+
 while True:
-    result, image = cam.read()
-    cv2.imshow("Say Cheese", image)
-    # cv2.imwrite("shubh.bmp", image)
+	sucess, imgOrignal=cap.read()
+	faces = facedetect.detectMultiScale(imgOrignal,1.3,5)
+	for x,y,w,h in faces:
+		crop_img=imgOrignal[y:y+h,x:x+h]
+		img=cv2.resize(crop_img, (224,224))
+		img=img.reshape(1, 224, 224, 3)
+		prediction=model.predict(img)
+		classIndex=model.predict_step(img)
+		print("classIndex",classIndex)
+		if classIndex[0]> classIndex[1]:
+			classIndex=0
+		else:
+			classIndex=1
+		print("class Index",classIndex)
+		probabilityValue=np.amax(prediction)
+		print("prob",probabilityValue)
+		if classIndex==0:
+			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
+			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
+			cv2.putText(imgOrignal, str(get_className(classIndex)),(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
+		elif classIndex==1:
+			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
+			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
+			cv2.putText(imgOrignal, str(get_className(classIndex)),(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
 
-    print("Image written to file-system : shubh.bmp")
+		cv2.putText(imgOrignal,str(round(probabilityValue*100, 2))+"%" ,(180, 75), font, 0.75, (255,0,0),2, cv2.LINE_AA)
+	cv2.imshow("Result",imgOrignal)
+	k=cv2.waitKey(1)
+	if k==ord('q'):
+		break
 
-    # Load the model
 
-    # Create the array of the right shape to feed into the keras model
-    # The 'length' or number of images you can put into the array is
-    # determined by the first position in the shape tuple, in this case 1.
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    # Replace this with the path to your image
-    image = Image.open('shubh.bmp').convert("RGB") #
-    #resize the image to a 224x224 with the same strategy as in TM2:
-    #resizing the image to be at least 224x224 and then cropping from the center
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+cap.release()
+cv2.destroyAllWindows()
 
-    #turn the image into a numpy array
-    image_array = np.asarray(image)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
-    data[0] = normalized_image_array
 
-    # run the inference
-    prediction = model.predict(data)
-    print("prediction",prediction[0][0]*100)
-    print("prediction",prediction)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
